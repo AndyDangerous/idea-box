2b2811879fa8c74c64cf1c75741f3a0abb6ac2e0
Andy Mention <amention@gmail.com>
2014-07-22 10:39:52 -0600

    CLEANUPDOZER

---
+++ lib/app.rb
@@ -0,0 +1,40 @@
+require 'bundler'
+require 'idea_box'
+Bundler.require
+
+class IdeaBoxApp < Sinatra::Base
+  set :method_override, true
+  set :root, 'lib/app'
+
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  not_found do
+    erb :error
+  end
+
+  get '/' do
+    erb :index, locals: {ideas: IdeaStore.all} # , idea: Idea.new}
+  end
+
+  post '/' do
+    idea = IdeaStore.create(params[:idea])
+    redirect '/'
+  end
+
+  delete '/:i/delete' do |i|
+    IdeaStore.delete(i.to_i)
+    redirect '/'
+  end
+
+  get '/:id/edit' do |id|
+    idea = IdeaStore.find(id.to_i)
+    erb :edit, locals: {id: id, idea: idea}
+  end
+
+  put '/:id' do |id|
+    IdeaStore.update(id.to_i, params[:idea])
+    redirect '/'
+  end
+end
